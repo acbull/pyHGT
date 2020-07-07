f1565b727a036d725b105f89fe38fcf59c7f5e2e
Ziniu Hu <31607751+acbull@users.noreply.github.com>
2020-07-07 14:26:17 -0700

    Update eval_ogbn_mag.py

--- ogbn-mag/eval_ogbn_mag.py
+++ ogbn-mag/eval_ogbn_mag.py
@@ -23,17 +23,50 @@ parser = argparse.ArgumentParser(description='Training GNN on ogbn-mag benchmark
 
 
 
-parser.add_argument('--input_dir', type=int, default=0,
-                    help='Avaiable GPU ID')
-parser.add_argument('--model_dir', type=int, default=0,
-                    help='Avaiable GPU ID')
-parser.add_argument('--task_type', type=str, default='ensemble,
+parser.add_argument('--data_dir', type=int, default='/datadrive/dataset/OGB_MAG.pk',
+                    help='The address of preprocessed graph.')
+parser.add_argument('--model_dir', type=int, default='./hgt_4layer',
+                    help='The address for storing the trained models.')
+parser.add_argument('--task_type', type=str, default='ensemble',
                     help='Whether to use ensemble evaluation or sequential evaluation')
+parser.add_argument('--ensemble_num', type=int, default=8,
+                    help='Whether to use ensemble evaluation or sequential evaluation')
+parser.add_argument('--n_pool', type=int, default=8,
+                    help='Number of process to sample subgraph')  
+parser.add_argument('--sample_depth', type=int, default=6,
+                    help='How many layers within a mini-batch subgraph')
+parser.add_argument('--sample_width', type=int, default=520,
+                    help='How many nodes to be sampled per layer per type')
+parser.add_argument('--n_batch', type=int, default=32,
+                    help='Number of batch (sampled graphs) for each epoch') 
+parser.add_argument('--batch_size', type=int, default=256,
+                    help='Number of output nodes for training')   
+
+
+
+parser.add_argument('--conv_name', type=str, default='hgt',
+                    choices=['hgt', 'gcn', 'gat', 'rgcn', 'han', 'hetgnn'],
+                    help='The name of GNN filter. By default is Heterogeneous Graph Transformer (hgt)')
+parser.add_argument('--n_hid', type=int, default=512,
+                    help='Number of hidden dimension')
+parser.add_argument('--n_heads', type=int, default=8,
+                    help='Number of attention head')
+parser.add_argument('--n_layers', type=int, default=4,
+                    help='Number of GNN layers')
+parser.add_argument('--dropout', type=int, default=0.2,
+                    help='Dropout ratio')
+parser.add_argument('--sample_depth', type=int, default=6,
+                    help='How many numbers to sample the graph')
+parser.add_argument('--sample_width', type=int, default=520,
+                    help='How many nodes to be sampled per layer per type')
+parser.add_argument('--prev_norm', help='Whether to add layer-norm on the previous layers', action='store_true')
+parser.add_argument('--last_norm', help='Whether to add layer-norm on the last layers',     action='store_true')
+parser.add_argument('--use_RTE',   help='Whether to use RTE',     action='store_true')
 
 args = parser.parse_args()
 args_print(args)
 
-graph = dill.load(open(args.input_dir, 'rb'))
+graph = dill.load(open(args.data_dir, 'rb'))
 
 
 def ogbn_mag_sample(seed, samp_nodes):
@@ -122,14 +155,14 @@ with torch.no_grad():
         y_pred = []
         y_true = []
         pool = mp.Pool(args.n_pool)
-        jobs = prepare_data(pool, task_type = 'sequential', s_idx = 0, n_batch = args.n_batch, batch_size=args.batch_size)
+        jobs = prepare_data(pool, task_type = 'sequential', s_idx = 0, n_batch = args.ensemble_num, batch_size=args.batch_size)
         with tqdm(np.arange(len(graph.test_paper) // args.batch_size), desc='eval') as monitor:
             for s_idx in monitor:
                 test_data = [job.get() for job in jobs]
                 pool.close()
                 pool.join()
                 pool = mp.Pool(args.n_pool)
-                jobs = prepare_data(pool, is_test = True, s_idx = int(s_idx * args.n_batch), batch_size=args.batch_size)
+                jobs = prepare_data(pool, is_test = True, s_idx = int(s_idx * args.ensemble_num), batch_size=args.batch_size)
 
                 for node_feature, node_type, edge_time, edge_index, edge_type, (train_mask, valid_mask, test_mask), ylabel in test_data:
                     ylabel = ylabel[:args.batch_size]
