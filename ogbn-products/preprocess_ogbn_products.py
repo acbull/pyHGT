aa055d5dd27ddb60d686a2331ced83a4eea67d9b
acbull <bull@cs.ucla.edu>
2020-07-08 23:42:40 +0000

    product

--- ogbn-products/preprocess_ogbn_products.py
+++ ogbn-products/preprocess_ogbn_products.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser(description='Preprocess ogbn-products graph')
 '''
     Dataset arguments
 '''
-parser.add_argument('--data_dir', type=str, default='/datadrive/dataset',
+parser.add_argument('--data_dir', type=str, default='./',
                     help='The address to output the preprocessed graph.')
 parser.add_argument('--data_name', type=str, default='ogbn-products',
                     help='Name of the dataset')
@@ -17,17 +17,48 @@ parser.add_argument('--data_name', type=str, default='ogbn-products',
 args = parser.parse_args()
 
 
-dataset = PygNodePropPredDataset(name=args.data_name)
+dataset = PygNodePropPredDataset(name=args.data_name, root = args.data_dir)
 data = dataset[0]
 graph = Graph()
 
-elist = graph.edge_list['def']['def']['def']
+y = data.y.t().numpy()[0]
+split_idx = dataset.get_idx_split()
+train_paper = split_idx['train'].numpy()
+valid_paper = split_idx['valid'].numpy()
+test_paper  = split_idx['test'].numpy()
+
+graph.y = data.y
+graph.train_paper = train_paper
+graph.valid_paper = valid_paper
+graph.test_paper  = test_paper
+
+
+graph.train_mask = np.zeros(len(graph.y), dtype=bool)
+graph.train_mask[graph.train_paper] = True
+
+graph.valid_mask = np.zeros(len(graph.y), dtype=bool)
+graph.valid_mask[graph.valid_paper] = True
+
+graph.test_mask = np.zeros(len(graph.y),  dtype=bool)
+graph.test_mask[graph.test_paper] = True
+
+
+elist = graph.edge_list['product']['product']['relate']
 for s_id, t_id in tqdm(data.edge_index.t().tolist()):
     elist[t_id][s_id] = 1
     elist[s_id][t_id] = 1
     
+elist = graph.edge_list['product']['cate']['belong']
+rlist = graph.edge_list['cate']['product']['rev_belong']
+
+for s_id, t_id in enumerate(y):
+    if not graph.test_mask[s_id]:
+        elist[s_id][t_id] = 1
+        rlist[t_id][s_id] = 1
+        
+        
 edg = {}
-deg = {'def' : np.zeros(len(data.y))}
+deg = {'product' : np.zeros(len(data.y)), 'cate' : np.zeros(len(data.y))}
 for k1 in graph.edge_list:
     if k1 not in edg:
         edg[k1] = {}
@@ -40,7 +71,6 @@ for k1 in graph.edge_list:
             for e1 in graph.edge_list[k1][k2][k3]:
                 if len(graph.edge_list[k1][k2][k3][e1]) == 0:
                     continue
-                
                 edg[k1][k2][k3][e1] = {}
                 for e2 in graph.edge_list[k1][k2][k3][e1]:
                     edg[k1][k2][k3][e1][e2] = graph.edge_list[k1][k2][k3][e1][e2]
@@ -49,26 +79,9 @@ for k1 in graph.edge_list:
 graph.edge_list = edg
 
 
-
-split_idx = dataset.get_idx_split()
-graph.train_node = split_idx['train'].numpy()
-graph.valid_node = split_idx['valid'].numpy()
-graph.test_node  = split_idx['test'].numpy()
-
-
 cv = data.x.numpy()
-graph.node_feature['def'] = np.concatenate((cv, np.log10((1e-10 + deg['def']).reshape(-1, 1))), axis=-1)
-graph.y = data.y
+graph.node_feature = {}
+graph.node_feature['product'] = np.concatenate((cv, np.log10((1e-3 + deg['product']).reshape(-1, 1))), axis=-1)
+graph.node_feature['cate']    = None
 
-
-graph.train_mask = np.zeros(len(graph.node_feature['def']), dtype=bool)
-graph.train_mask[graph.train_paper] = True
-
-graph.valid_mask = np.zeros(len(graph.node_feature['def']), dtype=bool)
-graph.valid_mask[graph.valid_paper] = True
-
-graph.test_mask = np.zeros(len(graph.node_feature['def']),  dtype=bool)
-graph.test_mask[graph.test_paper] = True
-
-
-dill.dump(graph, open('%s/%s.pk' % (args.data_dir, args.data_name), 'wb'))
+dill.dump(graph, open(args.data_dir + args.data_name, 'wb'))
--- ogbn-products/train_ogbn_products.py
+++ ogbn-products/train_ogbn_products.py
@@ -2,10 +2,8 @@ import argparse
 from tqdm import tqdm
 import sys
 
-from sklearn.metrics import f1_score
-from data import *
-from utils import *
-from model import *
+from pyHGT.data import *
+from pyHGT.model import *
 from warnings import filterwarnings
 filterwarnings("ignore")
 
@@ -23,13 +21,13 @@ parser = argparse.ArgumentParser(description='Training GNN on ogbn-products benc
 
 
 
-parser.add_argument('--data_dir', type=str, default='/datadrive/dataset',
+parser.add_argument('--data_dir', type=str, default='./',
                     help='The address to output the preprocessed graph.')
 parser.add_argument('--data_name', type=str, default='ogbn-products',
                     help='Name of the dataset')
-parser.add_argument('--model_dir', type=int, default='./hgt_6layer',
+parser.add_argument('--model_dir', type=str, default='./hello',
                     help='The address for storing the trained models.')
-parser.add_argument('--plot', type=int, action='store_true',
+parser.add_argument('--plot', action='store_true',
                     help='Whether to plot the loss/acc curve')
 parser.add_argument('--cuda', type=int, default=0,
                     help='Avaiable GPU ID')
@@ -40,13 +38,13 @@ parser.add_argument('--n_hid', type=int, default=256,
                     help='Number of hidden dimension')
 parser.add_argument('--n_heads', type=int, default=4,
                     help='Number of attention head')
-parser.add_argument('--n_layers', type=int, default=6,
+parser.add_argument('--n_layers', type=int, default=3,
                     help='Number of GNN layers')
 parser.add_argument('--dropout', type=int, default=0.2,
                     help='Dropout ratio')
-parser.add_argument('--sample_depth', type=int, default=7,
+parser.add_argument('--sample_depth', type=int, default=6,
                     help='How many numbers to sample the graph')
-parser.add_argument('--sample_width', type=int, default=768,
+parser.add_argument('--sample_width', type=int, default=2048,
                     help='How many nodes to be sampled per layer per type')
 parser.add_argument('--prev_norm', help='Whether to add layer-norm on the previous layers', action='store_true')
 parser.add_argument('--last_norm', help='Whether to add layer-norm on the last layers',     action='store_true')
@@ -58,13 +56,13 @@ parser.add_argument('--use_RTE',   help='Whether to use RTE',     action='store_
 parser.add_argument('--optimizer', type=str, default='adamw',
                     choices=['adamw', 'adam', 'sgd', 'adagrad'],
                     help='optimizer to use.')
-parser.add_argument('--n_epoch', type=int, default=100,
+parser.add_argument('--n_epoch', type=int, default=200,
                     help='Number of epoch to run')
 parser.add_argument('--n_pool', type=int, default=8,
                     help='Number of process to sample subgraph')    
 parser.add_argument('--n_batch', type=int, default=32,
                     help='Number of batch (sampled graphs) for each epoch') 
-parser.add_argument('--batch_size', type=int, default=128,
+parser.add_argument('--batch_size', type=int, default=256,
                     help='Number of output nodes for training')    
 parser.add_argument('--clip', type=int, default=1.0,
                     help='Gradient Norm Clipping') 
@@ -72,51 +70,122 @@ parser.add_argument('--clip', type=int, default=1.0,
 args = parser.parse_args()
 args_print(args)
 
-def ogbn_sample(seed, samp_nodes):
+def feature_products(layer_data, graph):
+    feature = {}
+    times   = {}
+    indxs   = {}
+    texts   = []
+    for _type in layer_data:
+        if len(layer_data[_type]) == 0:
+            continue
+        idxs  = np.array(list(layer_data[_type].keys()), dtype = np.int)
+        tims  = np.array(list(layer_data[_type].values()))[:,1]
+        if _type == 'cate':
+            feature[_type] = np.zeros([len(idxs), len(graph.node_feature['product'][0])])
+            feature[_type][:,0] = idxs
+        else:
+            feature[_type] = graph.node_feature[_type][idxs]
+        times[_type]   = tims
+        indxs[_type]   = idxs
+        
+    return feature, times, indxs, texts
+
+def node_classification_sample(seed, samp_nodes):
+    '''
+        sub-graph sampling and label preparation for node classification:
+        (1) Sample batch_size number of output nodes (papers), get their time.
+    '''
     np.random.seed(seed)
-    ylabel      = torch.LongTensor(graph.y[samp_nodes])
-    feature, times, edge_list, indxs, _ = sample_subgraph(graph, \
-                inp = {'def': np.concatenate([samp_nodes, graph.years[samp_nodes]]).reshape(2, -1).transpose()}, \
+    feature, times, edge_list, _, _ = sample_subgraph(graph, \
+                inp = {'product': np.concatenate([samp_nodes, np.ones(len(samp_nodes))]).reshape(2, -1).transpose()}, \
                 sampled_depth = args.sample_depth, sampled_number = args.sample_width, \
-                    feature_extractor = feature_MAG)
+                    feature_extractor = feature_products)
+
+    masked_edge_list = []
+    for i in edge_list['product']['cate']['belong']:
+        if i[0] >= args.batch_size:
+            masked_edge_list += [i]
+    edge_list['product']['cate']['belong'] = masked_edge_list
+
+    masked_edge_list = []
+    for i in edge_list['cate']['product']['rev_belong']:
+        if i[1] >= args.batch_size:
+            masked_edge_list += [i]
+    edge_list['cate']['product']['rev_belong'] = masked_edge_list
+
     node_feature, node_type, edge_time, edge_index, edge_type, node_dict, edge_dict = \
             to_torch(feature, times, edge_list, graph)
-    train_mask = graph.train_mask[indxs['def']]
-    valid_mask = graph.valid_mask[indxs['def']]
-    test_mask  = graph.test_mask[indxs['def']]
-    ylabel     = graph.y[indxs['def']]
-    return node_feature, node_type, edge_time, edge_index, edge_type, (train_mask, valid_mask, test_mask), ylabel
     
-def prepare_data(pool, task_type = 'train', s_idx = 0, n_batch = args.n_batch, batch_size = args.batch_size):
+    ylabel = torch.LongTensor(graph.y[samp_nodes])
+    x_ids = np.arange(args.batch_size)
+    return node_feature, node_type, edge_time, edge_index, edge_type, x_ids, ylabel
+
+
+def prepare_data(pool):
     '''
         Sampled and prepare training and validation data using multi-process parallization.
     '''
     jobs = []
-    if task_type == 'train':
-        for batch_id in np.arange(n_batch):
-            p = pool.apply_async(ogbn_sample, args=([randint(), \
-                            np.random.choice(graph.train_paper, args.batch_size, replace = False)]))
-            jobs.append(p)
-    elif task_type == 'sequential':
-        for i in np.arange(n_batch):
-            target_papers = graph.test_paper[(s_idx + i) * batch_size : (s_idx + i + 1) * batch_size]
-            p = pool.apply_async(ogbn_sample, args=([randint(), target_papers]))
-            jobs.append(p)
-    elif task_type == 'variance_reduce':
-        target_papers = graph.test_paper[s_idx * args.batch_size : (s_idx + 1) * args.batch_size]
-        for batch_id in np.arange(n_batch):
-            p = pool.apply_async(ogbn_sample, args=([randint(), target_papers]))
-            jobs.append(p)
+    for batch_id in np.arange(args.n_batch):
+        p = pool.apply_async(node_classification_sample, args=(randint(), \
+            np.random.choice(graph.train_paper, args.batch_size, replace = False)))
+        jobs.append(p)
+    p = pool.apply_async(node_classification_sample, args=(randint(), \
+           np.random.choice(graph.valid_paper, args.batch_size, replace = False)))
+    jobs.append(p)
+    
+    p = pool.apply_async(node_classification_sample, args=(randint(), \
+               np.random.choice(graph.test_paper, args.batch_size, replace = False)))
+    jobs.append(p)
     return jobs
 
-graph = dill.load(open('%s/%s.pk' % (args.data_dir, args.data_name), 'rb'))
+    class GNN(nn.Module):
+        def __init__(self, in_dim, n_hid, num_types, num_relations, n_heads, n_layers,\
+                     dropout = 0.2, conv_name = 'hgt', prev_norm = False, last_norm = False, use_RTE = True):
+            super(GNN, self).__init__()
+            self.gcs = nn.ModuleList()
+            self.num_types = num_types
+            self.in_dim    = in_dim
+            self.n_hid     = n_hid
+            self.adapt_ws  = nn.ModuleList()
+            self.drop      = nn.Dropout(dropout)
+            for t_id in range(num_types):
+                if graph.get_types()[t_id] == 'cate':
+                    self.adapt_ws.append(nn.Embedding(graph.y.max().item()+1, n_hid))
+                else:
+                    self.adapt_ws.append(nn.Linear(in_dim, n_hid))
+            for l in range(n_layers - 1):
+                self.gcs.append(GeneralConv(conv_name, n_hid, n_hid, num_types, num_relations, n_heads, dropout, use_norm = prev_norm, use_RTE = use_RTE))
+            self.gcs.append(GeneralConv(conv_name, n_hid, n_hid, num_types, num_relations, n_heads, dropout, use_norm = last_norm, use_RTE = use_RTE))
+
+        def forward(self, node_feature, node_type, edge_time, edge_index, edge_type):
+            res = torch.zeros(node_feature.size(0), self.n_hid).to(node_feature.device)
+            for t_id in range(self.num_types):
+                idx = (node_type == int(t_id))
+                if idx.sum() == 0:
+                    continue
+                if graph.get_types()[t_id] == 'cate':
+                    res[idx] = self.adapt_ws[t_id](node_feature[idx][:,0].long())
+                else:
+                    res[idx] = F.gelu(self.adapt_ws[t_id](node_feature[idx]))
+            meta_xs = self.drop(res)
+            del res
+            for gc in self.gcs:
+                meta_xs = gc(meta_xs, node_type, edge_index, edge_type, edge_time)
+            return meta_xs  
+        
+        
+
+
+graph = dill.load(open(os.path.join(args.data_dir, args.data_name + '.pk'), 'rb'))
 evaluator = Evaluator(name=args.data_name)
 device = torch.device("cuda:%d" % args.cuda)
-gnn = GNN(conv_name = args.conv_name, in_dim = len(graph.node_feature['paper'][0]), \
+print(graph.node_feature.keys())
+gnn = GNN(conv_name = args.conv_name, in_dim = len(graph.node_feature['product'][0]), \
           n_hid = args.n_hid, n_heads = args.n_heads, n_layers = args.n_layers, dropout = args.dropout,\
           num_types = len(graph.get_types()), num_relations = len(graph.get_meta_graph()) + 1,\
           prev_norm = args.prev_norm, last_norm = args.last_norm, use_RTE = args.use_RTE)
-classifier = Classifier(args.n_hid, graph.y.max()+1)
+classifier = Classifier(args.n_hid, graph.y.max().item()+1)
 
 model = nn.Sequential(gnn, classifier).to(device)
 print('Model #Params: %d' % get_n_params(model))
@@ -133,7 +202,7 @@ optimizer_grouped_parameters = [
 
 optimizer = torch.optim.AdamW(optimizer_grouped_parameters, eps=1e-06)
 scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, pct_start=0.05, anneal_strategy='linear', final_div_factor=10,\
-                        max_lr = 1e-3, total_steps = args.n_batch * args.n_epoch + 1)
+                        max_lr = 5e-4, total_steps = args.n_batch * args.n_epoch + 1)
 
 stats = []
 res   = []
@@ -149,7 +218,9 @@ for epoch in np.arange(args.n_epoch) + 1:
     '''
         Prepare Training and Validation Data
     '''
-    datas = [job.get() for job in jobs]
+    train_data = [job.get() for job in jobs[:-2]]
+    valid_data = jobs[-2].get()
+    test_data = jobs[-1].get()
     pool.close()
     pool.join()
     '''
@@ -164,59 +235,60 @@ for epoch in np.arange(args.n_epoch) + 1:
         Train
     '''
     model.train()
+    train_losses = []
     stat = []
-    for node_feature, node_type, edge_time, edge_index, edge_type, (train_mask, valid_mask, test_mask), ylabel in datas:
+    for node_feature, node_type, edge_time, edge_index, edge_type, x_ids, ylabel in train_data:
         node_rep = gnn.forward(node_feature.to(device), node_type.to(device), \
                                edge_time.to(device), edge_index.to(device), edge_type.to(device))
-        ylabel = torch.LongTensor(ylabel).to(device)
-        train_res  = classifier.forward(node_rep[:len(ylabel)][train_mask])
-        valid_res  = classifier.forward(node_rep[:len(ylabel)][valid_mask])
-        test_res   = classifier.forward(node_rep[:len(ylabel)][test_mask])
-
-        train_loss = criterion(train_res, ylabel[train_mask].squeeze())
+        res  = classifier.forward(node_rep[x_ids])
+        loss = criterion(res, ylabel.squeeze().to(device))
 
         optimizer.zero_grad() 
-        train_loss.backward()
+        torch.cuda.empty_cache()
+        loss.backward()
 
         torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
         optimizer.step()
 
+        train_losses += [loss.cpu().detach().tolist()]
         train_step += 1
         scheduler.step(train_step)
-
-        train_acc  = evaluator.eval({
-                        'y_true': ylabel[train_mask],
-                        'y_pred': train_res.argmax(dim=1).unsqueeze(-1)
-                    })['acc']
+        del node_rep, ylabel
+        
+    model.eval()
+    with torch.no_grad():
+        node_feature, node_type, edge_time, edge_index, edge_type, x_ids, ylabel = valid_data
+        node_rep = gnn.forward(node_feature.to(device), node_type.to(device), \
+                                   edge_time.to(device), edge_index.to(device), edge_type.to(device))
+        res  = classifier.forward(node_rep[x_ids])
+        loss = criterion(res, ylabel.squeeze().to(device))
+        
+        '''
+            Calculate Valid NDCG. Update the best model based on highest NDCG score.
+        '''
         valid_acc  = evaluator.eval({
-                        'y_true': ylabel[valid_mask],
-                        'y_pred': valid_res.argmax(dim=1).unsqueeze(-1)
+                        'y_true': ylabel,
+                        'y_pred': res.argmax(dim=1).unsqueeze(-1)
                     })['acc']
-        test_acc   = evaluator.eval({
-                        'y_true': ylabel[test_mask],
-                        'y_pred': test_res.argmax(dim=1).unsqueeze(-1)
+        
+        if valid_acc > best_val:
+            best_val = valid_acc
+            torch.save(model, args.model_dir)
+            print('UPDATE!!!')
+        
+        node_feature, node_type, edge_time, edge_index, edge_type, x_ids, ylabel = test_data
+        node_rep = gnn.forward(node_feature.to(device), node_type.to(device), \
+                                   edge_time.to(device), edge_index.to(device), edge_type.to(device))
+        res  = classifier.forward(node_rep[x_ids])
+        
+        test_acc  = evaluator.eval({
+                        'y_true': ylabel,
+                        'y_pred': res.argmax(dim=1).unsqueeze(-1)
                     })['acc']
-        stat += [[train_loss.item(), train_acc, valid_acc, test_acc]]
-        del node_rep, train_loss, ylabel
-    stats += [stat]
-    avgs = np.average(stat, axis=0)
-    if avgs[2] > best_val:
-        best_val = avgs[2]
-        torch.save(model.state_dict(), args.model_dir)
-        print('UPDATE!!!  ' + str(best_val))
-    print('Epoch: %d LR: %.5f Train Loss: %.4f Train Acc: %.4f Valid Acc: %.4f Test Acc: %.4f' % \
-         (epoch,  optimizer.param_groups[0]['lr'], avgs[0], avgs[1], avgs[2], avgs[3]))
-    st = time.time()
-    if args.plot and epoch % 50 == 0:
-        s = np.concatenate(stats)
-        for i in range(4):
-            data = np.stack((s[-args.n_batch * 100:, i], np.arange(len(s[-args.n_batch * 100:, i])) // args.batch_size), axis=0).transpose()
-            sb.lineplot(data = pd.DataFrame(data, columns = ['Value', 'Epoch']), x='Epoch', y='Value')
-            plt.show()
-            
-if args.plot:
-    s = np.concatenate(stats)
-    for i in range(4):
-        data = np.stack((s[args.n_batch * 100:, i], np.arange(len(s[args.n_batch * 100:, i])) // args.n_batch), axis=0).transpose()
-        sb.lineplot(data = pd.DataFrame(data, columns = ['Value', 'Epoch']), x='Epoch', y='Value')
-        plt.show()
+        
+        st = time.time()
+        print(("Epoch: %d (%.1fs)  LR: %.5f Train Loss: %.2f  Valid Loss: %.2f  Valid Acc: %.4f  Test Acc: %.4f") % \
+              (epoch, (st-et), optimizer.param_groups[0]['lr'], np.average(train_losses), \
+                    loss.cpu().detach().tolist(), valid_acc, test_acc))
+        stats += [[np.average(train_losses), loss.cpu().detach().tolist()]]
+        del res, loss
