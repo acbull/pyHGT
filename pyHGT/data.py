5dfe3d9afdb98d521dca5b588cb7302f340bae77
Ziniu Hu <31607751+acbull@users.noreply.github.com>
2020-07-14 21:00:01 -0700

    Update data.py

--- pyHGT/data.py
+++ pyHGT/data.py
@@ -84,7 +84,130 @@ class Graph():
 
 
 
-time_range
+def sample_subgraph(graph, time_range, sampled_depth = 2, sampled_number = 8, inp = None, feature_extractor = feature_OAG):
+    '''
+        Sample Sub-Graph based on the connection of other nodes with currently sampled nodes
+        We maintain budgets for each node type, indexed by <node_id, time>.
+        Currently sampled nodes are stored in layer_data.
+        After nodes are sampled, we construct the sampled adjacancy matrix.
+    '''
+    layer_data  = defaultdict( #target_type
+                        lambda: {} # {target_id: [ser, time]}
+                    )
+    budget     = defaultdict( #source_type
+                                    lambda: defaultdict(  #source_id
+                                        lambda: [0., 0] #[sampled_score, time]
+                            ))
+    new_layer_adj  = defaultdict( #target_type
+                                    lambda: defaultdict(  #source_type
+                                        lambda: defaultdict(  #relation_type
+                                            lambda: [] #[target_id, source_id]
+                                )))
+    '''
+        For each node being sampled, we find out all its neighborhood, 
+        adding the degree count of these nodes in the budget.
+        Note that there exist some nodes that have many neighborhoods
+        (such as fields, venues), for those case, we only consider 
+    '''
+    def add_budget(te, target_id, target_time, layer_data, budget):
+        for source_type in te:
+            tes = te[source_type]
+            for relation_type in tes:
+                if relation_type == 'self' or target_id not in tes[relation_type]:
+                    continue
+                adl = tes[relation_type][target_id]
+                if len(adl) < sampled_number:
+                    sampled_ids = list(adl.keys())
+                else:
+                    sampled_ids = np.random.choice(list(adl.keys()), sampled_number, replace = False)
+                for source_id in sampled_ids:
+                    source_time = adl[source_id]
+                    if source_time == None:
+                        source_time = target_time
+                    if source_time > np.max(list(time_range.keys())) or source_id in layer_data[source_type]:
+                        continue
+                    budget[source_type][source_id][0] += 1. / len(sampled_ids)
+                    budget[source_type][source_id][1] = source_time
+
+    '''
+        First adding the sampled nodes then updating budget.
+    '''
+    for _type in inp:
+        for _id, _time in inp[_type]:
+            layer_data[_type][_id] = [len(layer_data[_type]), _time]
+    for _type in inp:
+        te = graph.edge_list[_type]
+        for _id, _time in inp[_type]:
+            add_budget(te, _id, _time, layer_data, budget)
+    '''
+        We recursively expand the sampled graph by sampled_depth.
+        Each time we sample a fixed number of nodes for each budget,
+        based on the accumulated degree.
+    '''
+    for layer in range(sampled_depth):
+        sts = list(budget.keys())
+        for source_type in sts:
+            te = graph.edge_list[source_type]
+            keys  = np.array(list(budget[source_type].keys()))
+            if sampled_number > len(keys):
+                '''
+                    Directly sample all the nodes
+                '''
+                sampled_ids = np.arange(len(keys))
+            else:
+                '''
+                    Sample based on accumulated degree
+                '''
+                score = np.array(list(budget[source_type].values()))[:,0] ** 2
+                score = score / np.sum(score)
+                sampled_ids = np.random.choice(len(score), sampled_number, p = score, replace = False) 
+            sampled_keys = keys[sampled_ids]
+            '''
+                First adding the sampled nodes then updating budget.
+            '''
+            for k in sampled_keys:
+                layer_data[source_type][k] = [len(layer_data[source_type]), budget[source_type][k][1]]
+            for k in sampled_keys:
+                add_budget(te, k, budget[source_type][k][1], layer_data, budget)
+                budget[source_type].pop(k)   
+    '''
+        Prepare feature, time and adjacency matrix for the sampled graph
+    '''
+    feature, times, indxs, texts = feature_extractor(layer_data, graph)
+            
+    edge_list = defaultdict( #target_type
+                        lambda: defaultdict(  #source_type
+                            lambda: defaultdict(  #relation_type
+                                lambda: [] # [target_id, source_id] 
+                                    )))
+    for _type in layer_data:
+        for _key in layer_data[_type]:
+            _ser = layer_data[_type][_key][0]
+            edge_list[_type][_type]['self'] += [[_ser, _ser]]
+    '''
+        Reconstruct sampled adjacancy matrix by checking whether each
+        link exist in the original graph
+    '''
+    for target_type in graph.edge_list:
+        te = graph.edge_list[target_type]
+        tld = layer_data[target_type]
+        for source_type in te:
+            tes = te[source_type]
+            sld  = layer_data[source_type]
+            for relation_type in tes:
+                tesr = tes[relation_type]
+                for target_key in tld:
+                    if target_key not in tesr:
+                        continue
+                    target_ser = tld[target_key][0]
+                    for source_key in tesr[target_key]:
+                        '''
+                            Check whether each link (target_id, source_id) exist in original adjacancy matrix
+                        '''
+                        if source_key in sld:
+                            source_ser = sld[source_key][0]
+                            edge_list[target_type][source_type][relation_type] += [[target_ser, source_ser]]
+    return feature, times, edge_list, indxs, texts
 
 def to_torch(feature, time, edge_list, graph):
     '''
