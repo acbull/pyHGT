d40b377a49c7a820b01a5fbe791e598af4fa1433
Ziniu Hu <31607751+acbull@users.noreply.github.com>
2020-12-07 19:03:25 -0800

    Update eval_ogbn_mag.py

--- ogbn-mag/eval_ogbn_mag.py
+++ ogbn-mag/eval_ogbn_mag.py
@@ -100,10 +100,11 @@ def prepare_data(pool, task_type = 'train', s_idx = 0, n_batch = args.n_batch, b
             jobs.append(p)
     return jobs
 
+graph = dill.load(open(args.data_dir, 'rb'))
 np.random.seed(43)
 np.random.shuffle(graph.test_paper)
 
-graph = dill.load(open(args.data_dir, 'rb'))
+
 evaluator = Evaluator(name='ogbn-mag')
 device = torch.device("cuda:%d" % args.cuda)
 gnn = GNN(conv_name = args.conv_name, in_dim = len(graph.node_feature['paper'][0]), \
